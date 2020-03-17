6175e7abfe8fb353b3768d6ce656f9cfd19524eb
ljhang521 <ljhang521@sina.com>
2020-03-17 19:27:42 +0800

    improvement plan

--- src/main/java/cn/edu/upc/ljh/service/UserService.java
+++ src/main/java/cn/edu/upc/ljh/service/UserService.java
@@ -2,8 +2,9 @@ package cn.edu.upc.ljh.service;
 
 import cn.edu.upc.ljh.model.User;
 
+import java.util.List;
+
 public interface UserService {
 
     public User selectUser(int userId);
-    public List<User> selectUsersByKey(String username);
 }
