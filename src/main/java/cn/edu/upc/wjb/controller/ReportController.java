3ce47d24c2c519cb52155d2b81cdf2681c36d7c9
wangjiebing <782699317@qq.com>
2020-03-18 16:47:29 +0800

    wjb

---
+++ src/main/java/cn/edu/upc/wjb/controller/ReportController.java
@@ -0,0 +1,36 @@
+package cn.edu.upc.wjb.controller;
+
+import cn.edu.upc.manage.common.CommonReturnType;
+import cn.edu.upc.manage.model.Report;
+import cn.edu.upc.wjb.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(value = "/projectReport",method = RequestMethod.POST)
+@CrossOrigin(allowCredentials = "true",allowedHeaders = "*")
+public class ReportController {
+    @Autowired
+    ReportService reportService;
+
+    @RequestMapping("/getProjectReportList")
+    @ResponseBody
+    public CommonReturnType select(@RequestParam(value = "pid") int pid, @RequestParam(value = "gid") int gid, @RequestParam(value = "eid") int eid){
+        return CommonReturnType.create(reportService.getListByParam(pid,gid,eid));
+    }
+    @RequestMapping("/getProjectReportChickList")
+    @ResponseBody
+    public CommonReturnType checkSelect(@RequestParam(value = "pid") int pid, @RequestParam(value = "gid") int gid, @RequestParam(value = "eid") int eid){
+        return CommonReturnType.create(reportService.getListByParam(pid,gid,eid));
+    }
+
+    @RequestMapping("/insertProjectReportList")
+    @ResponseBody
+    public CommonReturnType insertNewMessage(@RequestBody Report report){
+        reportService.insertReport(report);
+        return CommonReturnType.create(null);
+
+    }
+
+}
