4f4b433f8ecd32ab814a5cee8deb11c68957138c
wenghaonan <wx_ae5873b9e90e46448f55e0851286313b@git.code.tencent.com>
2020-03-20 00:16:32 +0800

    wenghaonan 3200016 update test

--- src/main/java/cn/edu/upc/whn/service/impl/TestServiceImpl.java
+++ src/main/java/cn/edu/upc/whn/service/impl/TestServiceImpl.java
@@ -1,6 +1,8 @@
 package cn.edu.upc.whn.service.impl;
 
+import cn.edu.upc.manage.dao.MessageMapper;
 import cn.edu.upc.manage.dao.ViewMessageStateMapper;
+import cn.edu.upc.manage.model.Message;
 import cn.edu.upc.manage.model.ViewMessageState;
 import cn.edu.upc.whn.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +14,23 @@ import java.util.List;
 public class TestServiceImpl implements TestService {
     @Autowired
     ViewMessageStateMapper viewMessageStateMapper;
+    @Autowired
+    MessageMapper messageMapper;
 
     @Override
     public List<ViewMessageState> getMessageList(Integer id) {
         List<ViewMessageState> list = viewMessageStateMapper.getMessageList(id);
         return list;
+    }
+
+    @Override
+    public void updateReader(int id, int uid) {
+        Message result = messageMapper.selectByPrimaryKey(id);
+        if (result != null){
+            Message message = new Message();
+            message.setId(id);
+            message.setReader(result.getReader()+uid+',');
+            messageMapper.updateByPrimaryKeySelective(message);
+        }
     }
 }
