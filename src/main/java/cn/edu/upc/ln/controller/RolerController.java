60d3c37a7476b1949d9f1c9bcf01dcc90e0c1bbf
liuningsuper <751860499@qq.com>
2020-04-04 23:01:53 +0800

    刘宁role

--- src/main/java/cn/edu/upc/ln/controller/RolerController.java
+++ src/main/java/cn/edu/upc/ln/controller/RolerController.java
@@ -2,8 +2,9 @@ package cn.edu.upc.ln.controller;
 
 import cn.edu.upc.ln.service.RolerManageService;
 import cn.edu.upc.manage.common.CommonReturnType;
-import cn.edu.upc.manage.model.RolerManage;
-import cn.edu.upc.manage.model.Test;
+import cn.edu.upc.manage.model.*;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -20,32 +21,67 @@ public class RolerController {
     @RequestMapping("/Role")
     @ResponseBody
     public CommonReturnType disRoler(){
-        List<RolerManage> r1 = rolerManageService.selectAllRoler();
+        List<ViewRightsRole> r1 = rolerManageService.selectAllRoler();
         return CommonReturnType.create(r1);
     }
 
     @RequestMapping("/insertNewRole")
     @ResponseBody
-    public CommonReturnType insertNewRole(@RequestBody RolerManage para){
-        para.setDeleteMark(0);
-        rolerManageService.insertNewRoler(para);
-        List<RolerManage> r1=rolerManageService.selectAllRoler();
-        return CommonReturnType.create(r1,"新增成功");
+    public CommonReturnType insertNewRole(@RequestBody JSONObject para){
+        String roleName=para.getString("roleName");
+        JSONArray jsonArray = para.getJSONArray("rightsList");
+        int len=jsonArray.size();
+        Role role =new Role();
+        role.setRoleName(roleName);
+        role.setDelFlag(0);
+        int roleId=rolerManageService.insertNewRole(role);
+        for(int a=0;a<len;a++)
+        {
+            int rightId=jsonArray.getInteger(a);
+            RightRole rightRole =new RightRole();
+            rightRole.setRightId(rightId);
+            rightRole.setRoleId(roleId);
+            rightRole.setDelFlag(0);
+            rolerManageService.insertNewRightRole(rightRole);
+        }
+        return CommonReturnType.create(null,null,0,"新增成功");
     }
 
     @RequestMapping("/updateRole")
     @ResponseBody
-    public CommonReturnType updateRole(@RequestBody RolerManage para){
-        rolerManageService.updateRoler(para);
+    public CommonReturnType updateRole(@RequestBody JSONObject para){
+        int deleteId=para.getInteger("roleId");
+        rolerManageService.deleteRightRole(deleteId);
+        JSONArray jsonArray = para.getJSONArray("rightsList");
+        String roleName=para.getString("roleName");
+        RightRoleInput r=new RightRoleInput();
+        r.setId(deleteId);
+        r.setRoleName(roleName);
+        rolerManageService.updateRoleName(r);
+        int length=jsonArray.size();
+        for(int b=0;b<length;b++)
+        {
+            int rightId=jsonArray.getInteger(b);
+            RightRole rightRole =new RightRole();
+            rightRole.setRightId(rightId);
+            rightRole.setRoleId(deleteId);
+            rightRole.setDelFlag(0);
+            rolerManageService.insertNewRightRole(rightRole);
+        }
         return CommonReturnType.create(null,null,0,"修改成功");
     }
 
     @RequestMapping("/deleteRole")
     @ResponseBody
     public CommonReturnType deleteRole(@RequestBody Test para){
-        RolerManage r3=rolerManageService.selectRolerManage(para.getId());
-        r3.setDeleteMark(1);
-        rolerManageService.updateRoler(r3);
+        rolerManageService.delete(para.getId());
         return CommonReturnType.create(null,null,0,"删除成功");
+    }
+
+    @RequestMapping("/selectItem")
+    @ResponseBody
+    public CommonReturnType selectItem(@RequestBody Test para){
+        List<RightRole> r1=rolerManageService.selectByRoleId(para.getId());
+        return CommonReturnType.create(r1);
     }
 }
