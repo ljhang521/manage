47cb25665ad0be63bec04cad294db1f39805ca4a
hyz-yuan <1530189635@qq.com>
2020-04-04 17:00:54 +0800

    修改Controller中的json数据格式变换错误-hyz

--- src/main/java/cn/edu/upc/hyz/controller/TeamController.java
+++ src/main/java/cn/edu/upc/hyz/controller/TeamController.java
@@ -177,7 +177,7 @@ public class TeamController {
             lm.put("groupList",GroupList);
         }*/
         LinkedHashMap<String,Object> organizationStructure = dataHandling(technologys);
-        return CommonReturnType.create(technologys);
+        return CommonReturnType.create(organizationStructure);
     }
 
     public LinkedHashMap<String,Object> dataProcessing(List<LinkedHashMap<String,Object>> data){
